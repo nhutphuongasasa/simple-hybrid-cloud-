a9c3fa0174fe247c93392755c96deadd03a80ddc
unknown <phuongbt3232@gmail.com>
2026-06-01 04:12:43 +0700

    update screenshots

--- infrastructre/terraform/vpn.tf
+++ infrastructre/terraform/vpn.tf
@@ -12,12 +12,12 @@ resource "google_compute_external_vpn_gateway" "pfsense" {
 
   interface {
     id         = 0
-    ip_address = "123.21.249.22"  
+    ip_address = "123.21.141.108"  
   }
 
   interface {
     id         = 1
-    ip_address = "123.21.249.22"  
+    ip_address = "123.21.141.108"  
   }
 }
 
